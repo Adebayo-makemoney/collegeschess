e315375d1343ab5aff840b6f634db9b30aa4f16d
Adebayo-makemoney <aoluwafisayomi2022@gmail.com>
2025-08-15 13:14:55 +0100

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,7 @@
+function colorChange(){
+    document.getElementById("guide").style.display = "none "
+    document.getElementsByTagName("hr")[0].style.backgroundColor = "black"
+    document.getElementsByTagName("div")[0].style.color ="white"
+    
+}
+
